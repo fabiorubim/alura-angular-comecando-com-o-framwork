fb5ff00c03afd4ae11ec29cf1a559ab70d33adcd
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2021-12-08 14:01:18 -0300

    Data binding

--- src/app/nova-transfencia/nova-trasnferencia.component.ts
+++ src/app/nova-transfencia/nova-trasnferencia.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+
+  valor!: number;
+  destino!: number;
+
+  //ou
+  //valor: number = 12;
+  //destino: number = 222;
+
   transferir() {
     console.log('Solicitado nova transferência!');
+    console.log('Valor: R$', this.valor);
+    console.log('Destino: R$', this.destino);
   }
 }
