feae671ee65f753602e6b5056a727246ff93b512
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-01-11 23:48:44 -0300

    Utilizando ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ export class AppComponent {
   title = 'bytebank';
   // destino: number = 0;
   // valor: number = 0;
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event: any){
     console.log($event);
     // this.destino = $event.destino;
     // this.valor = $event.valor;
-    this.transferencia = $event;
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
   }
 }
